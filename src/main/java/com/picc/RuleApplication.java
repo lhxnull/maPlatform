d52bbc281a4701d92087b5d547f76cd3675255e9
lhxnull <571921459@qq.com>
2019-11-19 10:45:36 +0800

    11

--- src/main/java/com/picc/RuleApplication.java
+++ src/main/java/com/picc/RuleApplication.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.WebApplicationInitializer;
 
 /**
  * Created by lhx on 2019/9/29.
@@ -13,12 +15,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Slf4j
 @EnableTransactionManagement//事务支持
 @MapperScan("com.picc.*.mapper")
-public class RuleApplication {
+public class RuleApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
     public static void main(String[] args) {
         new SpringApplicationBuilder(RuleApplication.class).run(args);
     }
 
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RuleApplication.class);
+    }
 
 
 
